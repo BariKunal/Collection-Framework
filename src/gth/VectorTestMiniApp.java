92065570ea4a2fe63853af0ea3a353386148058a
BariKunal <kunalbari751@gmail.com>
2025-03-25 23:54:18 +0530

    new methods added

--- src/gth/VectorTestMiniApp.java
+++ src/gth/VectorTestMiniApp.java
@@ -11,6 +11,12 @@ public class VectorTestMiniApp {
 			System.out.println("2: View Vector Elements :");
 			System.out.println("3: Count Vector Elements :");
 			System.out.println("4: Search Vector Element by value:");
+			System.out.println("5:Search Element by index");
+			System.out.println("6:Delete by using its index");
+			System.out.println("7:Fetch elements by using get method");
+			System.out.println("8: SubList");
+			System.out.println("9: Remove element by value");
+
 			
 			System.out.println("Enter Your Choice:");
 			int ch = sc.nextInt();
@@ -58,6 +64,20 @@ public class VectorTestMiniApp {
 				break;
 				
 				case 5:
+					System.out.println("Enter The Search Index :");
+					value = sc.nextInt();
+					int ind = v.indexOf(value);
+					if(ind != -1)
+					{
+						System.out.println("Data Found..");
+					}
+					else
+					{
+						System.out.println("Data not Found...");
+					}
+				break;
+				
+				case 10:
 					System.exit(0);
 					break;
 				default :
