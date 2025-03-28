9c32164841884b784a07e87af9b4f5372556350e
BariKunal <kunalbari751@gmail.com>
2025-03-28 23:56:42 +0530

    Collections_class

---
+++ src/gth/Collections_classOperations.java
@@ -0,0 +1,26 @@
+package gth;
+import java.util.*;
+public class Collections_classOperations {
+
+	public static void main(String[] args) {
+		ArrayList al = new ArrayList();
+		al.add(5);
+		al.add(10);
+		al.add(100);
+		al.add(20);
+		al.add(50);
+		al.add(90);
+		al.add(40);
+		
+		Object maxVal = Collections.max(al);
+		System.out.println("Max value from ArrayList is "+maxVal);
+		Object minVal = Collections.min(al);
+		System.out.println("Min value from ArrayList is "+minVal);
+		Collections.sort(al);
+		System.out.println(al);
+		Collections.reverse(al);
+		System.out.println(al);
+
+	}
+
+}
