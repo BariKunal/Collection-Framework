f8bdbc88f1e9c406b4e01c41c19228c50a735966
BariKunal <kunalbari751@gmail.com>
2025-03-29 23:48:41 +0530

    Synchronized ArrayList

---
+++ src/gth/SynchronizedArrayList.java
@@ -0,0 +1,20 @@
+package gth;
+
+import java.util.*;
+
+public class SynchronizedArrayList {
+
+	public static void main(String[] args) {
+		List al = new ArrayList();
+		al.add("Kunal");
+		al.add(101);
+		al.add("Pune");
+		
+		List l=Collections.synchronizedList(al);	//It creates the arraylist synchronozed.
+		System.out.println(l);
+	
+		
+
+	}
+
+}
