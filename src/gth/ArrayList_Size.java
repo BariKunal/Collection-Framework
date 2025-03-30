7c8e1bc6d807937870ed5e6dfeed93510bb65b2e
BariKunal <kunalbari751@gmail.com>
2025-03-30 23:47:46 +0530

    size

--- src/gth/ArrayList_Size.java
+++ src/gth/ArrayList_Size.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class ArrayList_Size {
 
 	    public static void main(String[] args) {
-	        List<Double> prices = new ArrayList<>();
+	        List prices = new ArrayList();
 	        prices.add(19.99);
 	        prices.add(25.50);
 	        prices.add(12.75);
