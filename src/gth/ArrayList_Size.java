6570dd34f8a8764cca2bc29f0ffcb46620b7492a
BariKunal <kunalbari751@gmail.com>
2025-03-31 12:40:03 +0530

    format code

--- src/gth/ArrayList_Size.java
+++ src/gth/ArrayList_Size.java
@@ -1,14 +1,16 @@
 package gth;
+
 import java.util.*;
+
 public class ArrayList_Size {
 
-	    public static void main(String[] args) {
-	        List prices = new ArrayList();
-	        prices.add(19.99);
-	        prices.add(25.50);
-	        prices.add(12.75);
+	public static void main(String[] args) {
+		List prices = new ArrayList();
+		prices.add(19.99);
+		prices.add(25.50);
+		prices.add(12.75);
 
-	        System.out.println("Number of prices in the list: " + prices.size());
-	    }
-	
+		System.out.println("Number of prices in the list: " + prices.size());
+	}
+
 }
