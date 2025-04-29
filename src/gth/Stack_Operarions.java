1ff3fecde49b5b711573fe41c5e914a14f263af6
BariKunal <kunalbari751@gmail.com>
2025-04-29 23:36:34 +0530

    Delete(pop) method implemented

--- src/gth/Stack_Operarions.java
+++ src/gth/Stack_Operarions.java
@@ -8,17 +8,17 @@ public class Stack_Operarions {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
 		Stack s = new Stack();
-		
+
 		do {
 			System.out.println("1: Enter The Data In Stack ");
 			System.out.println("2: Print Stack Data ");
 			System.out.println("3: Remove Data In Stack ");
 			System.out.println("4: Exit Stack ");
-			
+
 			System.out.println("Enter Your Choice... ");
 			int ch = sc.nextInt();
-			
-			switch(ch) {
+
+			switch (ch) {
 			case 1:
 				System.out.println("Enter The Data : ");
 				int data = sc.nextInt();
@@ -27,20 +27,26 @@ public class Stack_Operarions {
 				break;
 			case 2:
 				ListIterator it = s.listIterator(s.size());
-				while(it.hasPrevious()) {
+				while (it.hasPrevious()) {
 					Object obj = it.previous();
 					System.out.println(obj);
 				}
 				break;
 			case 3:
-				
+				boolean b = s.isEmpty();
+				if (b) {
+					System.out.println("There is no data in stack");
+				} else {
+					System.out.println("Removed data is  " + s.pop());
+				}
+
 				break;
 			case 4:
 				break;
-				default :
-					System.out.println("Wrong Choice...");
+			default:
+				System.out.println("Wrong Choice...");
 			}
-		}while(true);
+		} while (true);
 	}
 
 }
